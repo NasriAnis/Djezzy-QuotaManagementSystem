d5f45af4e785e307cba08aa729dbd6f02df66427
NasriAnis <nasrianis213@gmail.com>
2026-08-27 14:04:16 +0100

    impl simple routes for main client page and /offers/<slug>/ detail page

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index_page, name="index_page"),
+    path("offers/<slug:offer_slug>/", views.offer_detail_page, name="offer_detail_page")
 ]
