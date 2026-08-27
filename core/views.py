24473898fdde6c7910905981cbe499742b0043cc
NasriAnis <nasrianis213@gmail.com>
2026-08-27 14:04:38 +0100

    impl / and /offers/<slug>/ views functions

--- core/views.py
+++ core/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Offer, OfferCategory, OfferPlan
 
+def index_page(request):
+    offers = Offer.objects.filter(is_active=True)
+    categories = OfferCategory.objects.all()
 
-def index(request):
-    return HttpResponse(b"Hello, world.")
+    # search / filter query logic here
+
+    context = {
+        'offers': offers,
+        'categories': categories,
+    }
+    return render(request, 'client_index.html', context)
+
+def offer_detail_page(request, offer_slug):
+    offer = get_object_or_404(Offer, slug=offer_slug, is_active=True)
+    offer_plans = offer.plans.all()
+
+    context = {
+        'offer': offer,
+        'offer_plans': offer_plans,
+    }
+    return render(request,"offer_details_page.html", context)
