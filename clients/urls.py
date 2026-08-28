db36d37315cd44ae5917c25c66f4d1d5c1cb711c
NasriAnis <nasrianis213@gmail.com>
2026-08-28 13:42:37 +0100

    setting up url paths for account/

--- clients/urls.py
+++ clients/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
+    path('signup/', views.client_signup, name='client_signup'),
+    path('login/', views.client_login, name='client_login'),
+    path('logout/', views.client_logout, name='client_logout'),
+    path('<str:username>/', views.client_account_page, name='client_account_page'),
 ]
--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("core.urls")),
-    path('/account', include("clients.urls")),
+    path('account/', include("clients.urls")),
 ]
 
 if settings.DEBUG:
