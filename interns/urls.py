b28d480b30c62ca50823f748d94a10f543531978
NasriAnis <nasrianis213@gmail.com>
2026-08-30 11:43:44 +0100

    impl urls for 'commercial/'

--- interns/urls.py
+++ interns/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.commercial_index_page, name='commercial_index_page'),
-    path('dashboard/', views.commercial_dashboard_page, name='commercial_dashboard_page'),
+    path('', views.commercials_index_page, name='commercials_index_page'),
+    path('login/', views.commercials_login, name='commercials_login'),
+    path('dashboard/', views.commercials_dashboard_page, name='commercials_dashboard_page'),
 ]
