8e453a5e83513ba5ed9a4a7a415502104041f93f
NasriAnis <nasrianis213@gmail.com>
2026-08-30 11:42:00 +0100

    impl DB for storing commercials intern info

--- interns/models.py
+++ interns/models.py
@@ -1,3 +1,26 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Commercial(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='commercial_profile',
+    )
+    phone = models.DecimalField(max_digits=10, decimal_places=0, unique=True)
+
+    class AccessRights(models.TextChoices):
+        READ_ONLY = "RO", "Read Only"
+        READ_WRITE = "RW", "Read and Write"
+
+    access_rights = models.CharField(
+        max_length=2,
+        choices=AccessRights.choices,
+        default=AccessRights.READ_ONLY,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.user.first_name} {self.user.last_name}"
