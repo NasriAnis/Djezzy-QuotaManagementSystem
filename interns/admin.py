7f410061bd51ba3995e0549a7751b32520192f18
NasriAnis <nasrianis213@gmail.com>
2026-08-30 11:43:15 +0100

    impl admin user commercial DB setup for adding an modifying accounts

--- interns/admin.py
+++ interns/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Commercial
+from .forms import CommercialAdminForm
 
-# Register your models here.
+@admin.register(Commercial)
+class CommercialAdmin(admin.ModelAdmin):
+    form = CommercialAdminForm
+    list_display = ('user', 'phone', 'access_rights', 'created_at')
+
+    def user(self, obj):
+        return f"{obj.user.first_name} {obj.user.last_name}"
