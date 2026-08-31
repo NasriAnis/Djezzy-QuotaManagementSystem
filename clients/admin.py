6c9bb9893689cfabcaff01a66def08e85f5fda65
NasriAnis <nasrianis213@gmail.com>
2026-08-31 21:22:31 +0100

    Added StoreOfferTransaction to admin panel

--- clients/admin.py
+++ clients/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Client, Store
+from .models import Client, Store, StoreOfferTransaction
 
 admin.site.register(Client)
 admin.site.register(Store)
+admin.site.register(StoreOfferTransaction)
