61f935381658c785e660e700bd8bb304e369bdaf
NasriAnis <nasrianis213@gmail.com>
2026-08-31 20:39:45 +0100

    OfferQuota DB for admin page

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import OfferCategory, Offer, OfferPlan
+from .models import OfferCategory, Offer, OfferPlan, OfferQuota
 
 admin.site.register(OfferCategory)
 admin.site.register(Offer)
 admin.site.register(OfferPlan)
+admin.site.register(OfferQuota)
